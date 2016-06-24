9481e1b0fd77093f801569fea16ec55d24efe307
MarkArts <markarts@hoppinger.com>
2016-06-24 13:58:50 +0200

    Accidently created a heart shape

--- tree/main.js
+++ tree/main.js
@@ -12,11 +12,11 @@ var controls = new THREE.OrbitControls( camera, renderer.domElement );
 // smooth my curve over this many points
 var numPoints = 20;
 
-function createLine(x, y, z, length, zrad, xrad)
+function createLine(x, y, z, length, zrad, yrad)
 {
   var points = [
     new THREE.Vector3(0, 0, 0),
-    new THREE.Vector3(length/8, length*0.75, 0),
+    //new THREE.Vector3(length/8, length*0.75, 0),
     new THREE.Vector3(0, length, 0),
   ];
 
@@ -39,8 +39,8 @@ function createLine(x, y, z, length, zrad, xrad)
   line.position.y = y;
   line.position.z = z;
 
-  line.rotateZ(zrad);
-  line.rotateX(xrad);
+  line.rotateY(yrad * (Math.PI/180));
+  line.rotateZ(zrad * (Math.PI/180));
 
   // This is porabply not the proper way to store the length of the Line
   line.length = length;
@@ -53,14 +53,35 @@ function splitLine(line){
 
   var vector = new THREE.Vector3(0, line.length, 0);
   vector.applyAxisAngle(new THREE.Vector3(0, 0, 1), line.rotation.z);
-  vector.applyAxisAngle(new THREE.Vector3(1, 0, 0), line.rotation.x);
+  vector.applyAxisAngle(new THREE.Vector3(0, 1, 0), line.rotation.y);
   
   var endPoint = vector.add(line.position);
 
-  console.log(endPoint);
+//  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, line.rotation.z - 45 * (Math.PI/180),  line.rotation.x + 45 * (Math.PI/180)));
+ // lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, line.rotation.z + 45 * (Math.PI/180),  line.rotation.x + 45 * (Math.PI/180)));
+ // lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, line.rotation.z + 45 * (Math.PI/180),  line.rotation.x + 45 * (Math.PI/180)));
 
-  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, line.rotation.z - 45 * (Math.PI/180),  line.rotation.x - 45 * (Math.PI/180))) //line.rotation.y - 45 * (Math.PI/180)));
-  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, line.rotation.z + 45 * (Math.PI/180),  line.rotation.x + 45 * (Math.PI/180))) //line.rotation.y + 45 * (Math.PI/180)));
+
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 45, 0));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 45, 120));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 45, 240));
+
+
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 0, 120));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 20, 120));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 40, 120));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 60, 120));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 80, 120));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 100, 120));
+
+
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 0, 240));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 20, 240));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 40, 240));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 60, 240));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 80, 240));
+  lines.push(createLine(endPoint.x, endPoint.y, endPoint.z, line.length/1.3, 100, 240));
+
 
   return lines;
 }
@@ -71,14 +92,14 @@ function splitRec(line, times){
   }
 
   var lines = splitLine(line);
+  var nextLines = lines.map(l => splitRec(l, times-1));
 
-  return [line].concat(splitRec(lines[0], times-1)).concat(splitRec(lines[1], times-1));
+  return [line].concat([].concat.apply([], nextLines));
 }
 
 
-
 var line = createLine(0, -10, 0,  5, 0, 0);
-var lines = splitRec(line, 11);
+var lines = splitRec(line, 2);
 
 
 lines.map( l => scene.add(l));
