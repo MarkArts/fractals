5a6659ddfdca69382afabedcf2654805479fd05d
MarkArts <markarts@hoppinger.com>
2017-06-25 22:31:02 +0200

    Add helper functiosn for the hashes

--- mandelbox/options.js
+++ mandelbox/options.js
@@ -3,8 +3,18 @@ function setOptionsFromHash(options){
 	hash.split(';').forEach(x => {
 		key=x.split('=')[0];
 		val=x.split('=')[1];
-		if(key in options){
-			options[key] = parseInt(val);
+		if(key in options){		
+			options[key] = parseFloat(val);
 		}
 	})
+}
+
+function url(){
+	return "#"+Object.keys(options).reduce( (acc, k) => {
+   		return k+"="+options[k]+";"+acc;
+	}, "");
+}
+
+function cameraHash(){
+	return "#"+"cameraX="+camera.position.x+";"+"cameraY="+camera.position.y+";"+"cameraZ="+camera.position.z+";";
 }
